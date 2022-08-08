62b36d94367161e7b1a00819fe1b80fe7d1454b6
Brennan Beam <BrennanBeam@gmail.com>
2022-08-08 14:49:03 -0700

    add config

---
+++ WAT-DotNet-SDK/ClassLibrary1/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace usace.wat.plugin
+{
+    public class Config
+    {
+        public AWSConfig[] aws_configs { get; set; }
+        public AWSConfig PrimaryConfig() 
+        {
+            return aws_configs[0];
+        }
+    }
+}
