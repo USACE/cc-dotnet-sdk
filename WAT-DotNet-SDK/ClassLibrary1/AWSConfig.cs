f51572c83f4149c260de989bb18b9a5e48964d1f
Brennan Beam <BrennanBeam@gmail.com>
2022-08-08 14:46:52 -0700

    Initial Project setup and first class awsconfig

---
+++ WAT-DotNet-SDK/ClassLibrary1/AWSConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace usace.wat.plugin
+{
+    public class AWSConfig
+    {
+        public string aws_config_name { get; set; }
+        public bool is_primary_config { get; set; }
+        public string aws_access_key_id { get; set; }
+        public string aws_secret_access_key_id { get; set; }
+        public string  aws_region { get; set; }
+        public string aws_bucket { get; set; }
+        public bool aws_mock { get; set; }
+        public string aws_endpoint { get; set; }
+        public string aws_disable_ssl { get; set; }
+        public bool aws_force_path_style { get; set; }
+    }
+}
