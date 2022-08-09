fa4324c39200bdf3e0942c46ef9c6597e297a648
Brennan Beam <BrennanBeam@gmail.com>
2022-08-09 13:49:52 -0700

    resource info

---
+++ usace.wat.plugin/ResourceInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace usace.wat.plugin
+{
+    public class ResourceInfo
+    {
+        public StoreTypes Store { get; set; }
+        public string Root { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ usace.wat.plugin/ResourcedFileData.cs
@@ -0,0 +1,10 @@
+﻿namespace usace.wat.plugin
+{
+    public class ResourcedFileData
+    {
+        public string Id { get; set; }
+        public string Filename { get; set; }
+        public ResourceInfo Resource_info { get; set; }
+        public ResourcedInternalPathData[] Internal_paths { get; set; }
+    }
+}
