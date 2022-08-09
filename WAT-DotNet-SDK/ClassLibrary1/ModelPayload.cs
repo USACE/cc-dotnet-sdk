7ed3ec289c30e6053bbce779fc4dcc884b2875d4
Brennan Beam <BrennanBeam@gmail.com>
2022-08-09 13:20:23 -0700

    model payload

---
+++ WAT-DotNet-SDK/ClassLibrary1/ModelPayload.cs
@@ -0,0 +1,11 @@
+﻿namespace usace.wat.plugin
+{
+    public class ModelPayload
+    {
+        public string payload_id { get; set; }
+        public Model model { get; set; }
+        public int event_index { get; set; }
+        public ResourcedFileData[] inputs { get; set; }
+        public ResourcedFileData[] outputs { get; set; }
+    }
+}
