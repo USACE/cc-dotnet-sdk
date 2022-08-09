b4d775923ed011548b50c68613cbbe20231f7700
Brennan Beam <BrennanBeam@gmail.com>
2022-08-09 09:38:00 -0700

    add FileData

---
+++ WAT-DotNet-SDK/ClassLibrary1/FileData.cs
@@ -0,0 +1,9 @@
+﻿namespace usace.wat.plugin
+{
+    public class FileData
+    {
+        public string id { get; set; }
+        public string filename { get; set; }
+        public InternalPathData[] internal_paths { get; set; }
+    }
+}
