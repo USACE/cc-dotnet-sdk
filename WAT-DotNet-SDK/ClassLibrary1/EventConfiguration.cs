74e3b9528f4979f55bf2544440eedff43f454f62
Brennan Beam <BrennanBeam@gmail.com>
2022-08-09 09:36:17 -0700

    Seed set and event configuration

---
+++ WAT-DotNet-SDK/ClassLibrary1/EventConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace usace.wat.plugin
+{
+    public class EventConfiguration
+    {
+        private SeedSet[] seeds;
+        public int event_number { get; set; }
+        public int realization_number { get; set; }
+
+        public SeedSet GetSeedSet(string identifier)
+        {
+            foreach(var seedSet in seeds)
+            {
+                if(seedSet.identifier == identifier)
+                {
+                    return seedSet;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ WAT-DotNet-SDK/ClassLibrary1/SeedSet.cs
@@ -0,0 +1,9 @@
+﻿namespace usace.wat.plugin
+{
+    public class SeedSet
+    {
+        public string? identifier { get; set; }
+        public long event_seed { get; set; }
+        public long realization_seed { get; set; }
+    }
+}
