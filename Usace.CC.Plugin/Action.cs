d4a41a58ba3a6abb364d0a7c09cb8605e552b755
Will Lehman <william.p.lehman@usace.army.mil>
2024-04-30 10:45:03 -0500

    fxes #22

---
+++ Usace.CC.Plugin/Action.cs
@@ -0,0 +1,18 @@
+namespace Usace.CC.Plugin
+{
+    /// <summary>
+    /// Generic action -- like a process for a plugin to execute
+    /// used for declaritively describing processes to include and their order
+    /// 
+    /// examples: manipulate input files, run a simulation, manipulate output files
+    /// </summary>
+    public class Action
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        [JsonPropertyName("desc")]
+        public string Description { get; set; }
+        [JsonPropertyName("params")]
+        public Map<string,string> Parameters { get; set; }
+    }
+}
